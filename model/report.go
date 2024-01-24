f5432f2a9b6133ecc3b393210f3207745a7ee01e
M Dikri Kurniadi <mdikrikurniadi1234@gmail.com>
2024-01-24 16:23:17 +0700

    fix report model

--- model/report.go
+++ model/report.go
@@ -7,7 +7,6 @@ type Report struct {
 	User_id    string    `json:"user_id"`
 	Report     string    `json:"report"`
 	Task_id    string    `json:"task_id"`
-	Project_id string    `json:"project_id"`
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
 	Deleted_at time.Time `json:"deleted_at"`
