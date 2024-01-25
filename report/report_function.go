6c6cea6e7ac73b35dfc5db90119b3d496581a585
M Dikri Kurniadi <mdikrikurniadi1234@gmail.com>
2024-01-25 11:38:59 +0700

    add condition if file is exis, just write down to that file

--- report/report_function.go
+++ report/report_function.go
@@ -27,26 +27,58 @@ func (r *reportToTXT) WriteReport(report model.ShowReport) error {
 	if err := os.MkdirAll(folderPath, os.ModePerm); err != nil {
 		return err
 	}
+	// Mengecek apakah file untuk hari tersebut sudah ada atau belum
+	_, err := os.Stat(fileName)
+	if os.IsNotExist(err) {
+		// Jika file belum ada, buat file baru
+		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		defer file.Close()
 
-	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
+		writer := bufio.NewWriter(file)
+
+		// Menggunakan encoding/json untuk mengubah struct Report menjadi JSON
+		jsonData, err := json.Marshal(report.Content)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		writer.WriteString(fmt.Sprintf("Date: %s\n%s\n", report.Date.Format("2006-01-02"), jsonData))
+
+		writer.WriteString("\n")
+		writer.Flush()
+
+		fmt.Printf("File '%s' telah dibuat.\n", fileName)
+	} else if err == nil {
+		// Jika file sudah ada, gunakan file tersebut
+		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		writer := bufio.NewWriter(file)
+
+		// Menggunakan encoding/json untuk mengubah struct Report menjadi JSON
+		jsonData, err := json.Marshal(report.Content)
+		if err != nil {
+			return err
+		}
+
+		writer.WriteString(fmt.Sprintf("Date: %s\n%s\n", report.Date.Format("2006-01-02"), jsonData))
+
+		writer.WriteString("\n")
+		writer.Flush()
+
+		fmt.Printf("File '%s' has created.\n", fileName)
+	} else {
+		fmt.Println(err)
 		return err
 	}
-	defer file.Close()
 
-	writer := bufio.NewWriter(file)
-
-	// Menggunakan encoding/json untuk mengubah struct Report menjadi JSON
-	jsonData, err := json.Marshal(report.Content)
-	if err != nil {
-		return err
-	}
-
-	writer.WriteString(fmt.Sprintf("Date: %s\n%s\n", report.Date.Format("2006-01-02"), jsonData))
-
-	writer.WriteString("\n")
-	writer.Flush()
-
-	fmt.Printf("File '%s' has created.\n", fileName)
 	return nil
 }
